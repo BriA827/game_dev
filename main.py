3278281b9e6ab4c250011038bff26ae7ac21eecb
BriA827 <brigamer13@gmail.com>
2024-09-23 13:35:01 -0400

    commit

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ GREEN = (0,255,0)
 BLUE = (0,0,255)
 YELLOW = (238, 210, 2)
 ORANGE = (255, 69, 0)
-
-PLANET = (207, 193, 171)
+GREY = (120,120,120)
+PLANET = (250, 183, 90)
 
 FPS = 60
 
@@ -21,11 +21,18 @@ def draw_rect(x, y, color, canvas):
     pg.draw.rect(canvas, color, [x, y, 150,100])
 
 def star(x, y, color, canvas):
-    pg.draw.polygon(canvas, color, [[x+30,y-25],[x+40,y],[x+60,y],[x+45,y+15],[x+55,y+35],[x+30,y+25],[x+5,y+35],[x+15,y+15],[x,y],[x+20,y]])
+    pg.draw.polygon(canvas, color, [[x+30,y-20],[x+40,y],[x+60,y],[x+45,y+15],[x+55,y+35],[x+30,y+25],[x+5,y+35],[x+15,y+15],[x,y],[x+20,y]])
+
+def comet(x, y, color, canvas):
+    pg.draw.circle(canvas, color, [x,y], 10,10)
+    pg.draw.polygon(canvas, color, [[x,y-10], [x+15,y-35], [x+15,y-25], [x+55, y-60], [x+40,y-30], [x+50, y-30], [x+5, y]])
+
 #setup
 pg.init()
 
-screen = pg.display.set_mode([600, 400])
+width = 1000
+height = 700
+screen = pg.display.set_mode([width, height])
 clock = pg.time.Clock()
 
 playing = True 
@@ -35,13 +42,23 @@ title_font = pg.font.SysFont('comicsans', 13)
 title_text = 'The Everlasting Lord of Arcane Wisdom, Shouki no Kami, the Prodigal'
 title_img = title_font.render(title_text, True, BLACK)
 
-x_list = []
-y_list = []
-colors = []
-for n in range(0, random.randint(7,19)):
-    x_list.append(random.randint(0,500))
-    y_list.append(random.randint(50,300))
-    colors.append(random.choice([YELLOW, ORANGE, WHITE]))
+x_list1 = []
+y_list1 = []
+star_colors = []
+
+x_list2 = []
+y_list2 = []
+comet_colors = []
+
+for n in range(0, random.randint(10,19)):
+    x_list1.append(random.randint(0,width))
+    y_list1.append(random.randint(0,height))
+    star_colors.append(random.choice([YELLOW, WHITE]))
+
+for n in range(0, random.randint(10,14)):
+    x_list2.append(random.randint(0,width))
+    y_list2.append(random.randint(0,height))
+    comet_colors.append(random.choice([BLUE, ORANGE]))
 
 #main game loop
 while playing:
@@ -61,13 +78,35 @@ while playing:
     # screen.blit(title_img, (10,10))
 
     #draw
-    for n in range(0,len(x_list)):
-        star(x_list[n], y_list[n], colors[n], screen)
+    for n in range(0,len(x_list1)):
+        star(x_list1[n], y_list1[n], star_colors[n], screen)
 
-    pg.draw.arc(screen, WHITE, [480,30, 70,70], 8.5*pi/6, 4*pi/6, 3)
-    pg.draw.arc(screen, WHITE, [475,30, 70,70], 8.5*pi/6, 4*pi/6, 5)
+    for n in range(0,len(x_list2)):
+        comet(x_list2[n], y_list2[n], comet_colors[n], screen)
 
-    pg.draw.circle(screen, PLANET, [300, 200], 50, 50)
+    #moon
+    pg.draw.arc(screen, WHITE, [850,50, 80,80], 8.5*pi/6, 4*pi/6, 3)
+    pg.draw.arc(screen, WHITE, [845,50, 80,80], 8.5*pi/6, 4*pi/6, 5)
+
+    #planet
+    pg.draw.arc(screen, ORANGE, [350, 290, 300,40], .5*pi, .49*pi, 6)
+    pg.draw.circle(screen, PLANET, [500, 300], 80, 80)
+    pg.draw.arc(screen, ORANGE, [350, 290, 300,40], .9*pi, .1*pi, 6)
+
+    #alien
+    pg.draw.polygon(screen, GREEN, [[700,650], [720,550],[800,550],[820,650]])
+    pg.draw.polygon(screen, GREY, [[670,550], [720,500],[800,500],[850,550]])
+    pg.draw.arc(screen, GREY, [720,455,80,90], 0, 1*pi, 3)
+    pg.draw.rect(screen,GREEN, [750, 490, 20,10])
+    pg.draw.ellipse(screen,GREEN, [750, 460, 20, 30])
+    circ_x = 710
+    for n in range(0,5):
+        pg.draw.circle(screen, YELLOW, [circ_x, 535], 5,5)
+        circ_x += 25
+    eye_x = 755
+    for n in range(0,2):
+        pg.draw.circle(screen, BLACK, [eye_x, 475], 3,3)
+        eye_x += 10
     
     #update screen
     pg.display.flip()
