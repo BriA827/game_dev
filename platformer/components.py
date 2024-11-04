f8d995f6b41c0e49082510ab2077277823523809
BriA827 <brigamer13@gmail.com>
2024-11-04 13:34:26 -0500

    commit

---
+++ platformer/components.py
@@ -0,0 +1,41 @@
+import pygame as pg
+
+class Player:
+    def __init__(self,x,y,width,height,color,display) -> None:
+        self.self = self
+        self.x = x
+        self.y = y
+        self.width = width 
+        self.height = height
+        self.color = color
+        self.display = display
+        self.velo = 5
+        self.x_velo = 0
+
+    def move(self):
+        self.x += self.x_velo
+
+    def keys(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            self.x_velo = -1 * self.velo
+        elif keys[pg.K_RIGHT]:
+            self.x_velo = self.velo
+        else:
+            self.x_velo = 0
+    
+    def draw(self):
+        pg.draw.rect(self.display,self.color, [self.x,self.y, self.width, self.height])
+
+class Wall:
+    def __init__(self, x, y, width, height, color, display) -> None:
+        self.self = self
+        self.x = x
+        self.y = y
+        self.width = width 
+        self.height = height
+        self.color = color
+        self.display = display
+    
+    def draw(self):
+        pg.draw.rect(self.display, self.color, [self.x,self.y, self.width,self.height])
--- platformer/platformer.py
+++ platformer/platformer.py
@@ -1,2 +1,27 @@
 import pygame as pg
-from platformer.settings import *
+from settings import *
+from components import *
+
+screen = pg.display.set_mode([WIDTH, HEIGHT])
+clock = pg.time.Clock()
+
+me = Player(20,800,50,70,YELLOW,screen)
+brick = Player(600,800,50,70,RED,screen)
+
+playing = True
+
+while playing == True:
+    screen.fill(BLACK)
+    me.draw()
+    me.move()
+    me.keys()
+
+    brick.draw()
+
+    
+    pg.display.flip()
+    clock.tick(FPS)
+
+    for event in pg.event.get():
+            if event.type == pg.QUIT:
+                playing = False
