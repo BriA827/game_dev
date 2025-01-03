d834799db40841822a286b35407f8284e024d06b
BriA827 <brigamer13@gmail.com>
2025-01-03 12:49:59 -0500

    s

---
+++ sprite_game/main.py
@@ -0,0 +1,23 @@
+import pygame as pg
+from settings import *
+from components import *
+
+def game():
+    pg.init()
+    screen = pg.display.set_mode([WIDTH, HEIGHT])
+    clock = pg.time.Clock()
+
+    playing = True
+    
+    while playing == True:
+        screen.fill(BACK)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                playing = False
+
+play = True
+
+while play == True:
+   game()
+pg.quit()
---
+++ sprite_game/settings.py
@@ -0,0 +1,17 @@
+import pygame as pg
+
+WHITE = [255,255,255]
+BLACK = [0,0,0]
+RED = [255,0,0]
+GREEN = [0,255,0]
+BLUE = [0,0,255]
+YELLOW = [238, 210, 0]
+ORANGE = [255, 69, 0]
+GREY = [120,120,120]
+
+BACK = BLACK
+
+FPS = 60
+
+WIDTH = 1000
+HEIGHT = 700
