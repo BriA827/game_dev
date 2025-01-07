db1daa56e51affdfcb1474b140570008c269fd75
BriA827 <brigamer13@gmail.com>
2025-01-07 13:35:08 -0500

    c

--- sprite_game/components.py
+++ sprite_game/components.py
@@ -5,7 +5,9 @@ class SpriteSheet():
     def __init__(self, filename):
         self.spritesheet = pg.image.load(filename).convert()
 
-    def get_image(self, x, y, width, height):
+    def get_image(self, x, y, width, height, scale_x = None, scale_y = None):
         image = pg.Surface((width, height))
         image.blit(self.spritesheet, (0,0), (x,y, width,height))
+        if scale_x and scale_y:
+            image = pg.transform.scale(image, (width*scale_x, height*scale_y))
         return image
--- sprite_game/main.py
+++ sprite_game/main.py
@@ -20,14 +20,64 @@ def game():
             explosion_list.append(image)
 
     tile_sheet = SpriteSheet("sprite_game/sprites/tilemap.png")
-    bow_image = tile_sheet.get_image(16*10+11, 16*11+10, 16, 16)
+    bow_image = tile_sheet.get_image(11*16-7, 9*16+9, 16, 16, 2, 2)
     bow_image.set_colorkey(BLACK) #looks for that color and makes transparent
+
+    grass_image = tile_sheet.get_image(17*2, 0. 16,16,4,4)
+
+    grass_sq = []
+    for y in range(1,4):
+        for x in range(0,3):
+            locx = 17 * x
+            locy = 17 * y
+            image = tile_sheet.get_image(locx, locy, 16, 16, 4, 4)
+            image.set_colorkey(BLACK)
+            grass_sq.append(image)
+
+    zombie_sheet = SpriteSheet("sprite_game/sprites/spritesheet_characters.png")
+    green_player_image = zombie_sheet.get_image(0,0,57,44)
+    green_player_image.set_colorkey(BLACK)
+    zombie_walk_image = zombie_sheet.get_image(425,0,37,44)
+    zombie_walk_image.set_colorkey(BLACK)
+
+    chars_sheet = SpriteSheet("sprite_game/sprites/new_chars.png")
+    king_still = chars_sheet.get_image(17,10*18-1, 32,46)
+    king_still.set_colorkey(NEW_CHARS)
+
     
     while playing == True:
         screen.fill(BACK)
+
+        for row in range(len(MAP)):
+            y_loc = row * (16*4)
+            for column in range(len(MAP[0])):
+                x_loc = column * (16*4)
+                if MAP[row][column] == "0":
+                    screen.blit(grass_sq[0], (x_loc, y_loc))
+                elif MAP[row][column] == "1":
+                    screen.blit(grass_sq[1], (x_loc, y_loc))
+                elif MAP[row][column] == "2":
+                    screen.blit(grass_sq[2], (x_loc, y_loc))
+                elif MAP[row][column] == "3":
+                    screen.blit(grass_sq[3], (x_loc, y_loc))
+                elif MAP[row][column] == "4":
+                    screen.blit(grass_sq[4], (x_loc, y_loc))
+                elif MAP[row][column] == "5":
+                    screen.blit(grass_sq[5], (x_loc, y_loc))
+                elif MAP[row][column] == "6":
+                    screen.blit(grass_sq[6], (x_loc, y_loc))
+                elif MAP[row][column] == "7":
+                    screen.blit(grass_sq[7], (x_loc, y_loc))
+                elif MAP[row][column] == "8":
+                    screen.blit(grass_sq[8], (x_loc, y_loc))
+                elif MAP[row][column] == "g":
+                    screen.blit(grass_sq[5], (x_loc, y_loc))
         
         screen.blit(explosion_list[0], (100,100))
-        screen.blit(bow_image, (200,200))
+        screen.blit(bow_image, (100,200))
+        screen.blit(green_player_image, (100,300))
+        screen.blit(zombie_walk_image, (100,400))
+        screen.blit(king_still, (100,500))
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
